eea150924fe2115d5a763b31dbb991c9c0b9bbdd
fakebenjay <fakebenjay@gmail.com>
2017-06-18 23:54:41 -0400

    Done.

--- lib/my_rotate.rb
+++ lib/my_rotate.rb
@@ -1,2 +1,21 @@
-# your code goes here
-# you will be monkey patching the Array class
+class Array
+  def my_rotate(a=1)
+    array = self
+    counter = 0
+
+    if a > 1
+      until counter == a
+        array.push(array[0])
+        array.shift
+        counter += 1
+      end
+    elsif a < 1
+      until counter == a
+        array.unshift(array[-1])
+        array.pop
+        counter -= 1
+      end
+    end
+    return array
+  end
+end
